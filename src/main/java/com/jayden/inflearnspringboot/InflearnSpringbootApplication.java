d619cc40431cf208b188cf729d5eff362758b5bb
174cm <omnia7@naver.com>
2020-08-06 11:23:12 +0900

    Initial commit

---
+++ src/main/java/com/jayden/inflearnspringboot/InflearnSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jayden.inflearnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayden/inflearnspringboot/InflearnSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayden.inflearnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
